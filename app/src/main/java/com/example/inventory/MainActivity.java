1c0750f6a79e5cfccfd859ec3e613ace6a93ebd6
HappyMercury <harshgup01@gmail.com>
2020-11-05 09:27:12 +0530

    Added fragments and setup basic navigation using bottom navigation bar

--- app/src/main/java/com/example/inventory/MainActivity.java
+++ app/src/main/java/com/example/inventory/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.inventory;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        DashboardFragment initialDashboardFragment = new DashboardFragment();
+        fragmentTransaction.replace(R.id.fragment_display,initialDashboardFragment);
+        fragmentTransaction.commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch(item.getItemId())
+                {
+                    case R.id.dashboard:
+                        item.setChecked(true);
+                        Toast.makeText(MainActivity.this, "Dashboard", Toast.LENGTH_SHORT).show();
+
+                        DashboardFragment dashboardFragment = new DashboardFragment();
+                        fragmentTransaction.replace(R.id.fragment_display,dashboardFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                        fragmentTransaction.addToBackStack("dashboard");
+                        fragmentTransaction.commit();
+                        break;
+                    case R.id.settings:
+                        item.setChecked(true);
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        SettingsFragment settingsFragment = new SettingsFragment();
+                        fragmentTransaction.replace(R.id.fragment_display,settingsFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                        fragmentTransaction.addToBackStack("settings");
+                        fragmentTransaction.commit();
+                        break;
+                    case R.id.todolist:
+                        item.setChecked(true);
+                        Toast.makeText(MainActivity.this, "TO-DO List", Toast.LENGTH_SHORT).show();
+                        ToDoListFragment toDoListFragment = new ToDoListFragment();
+                        fragmentTransaction.replace(R.id.fragment_display,toDoListFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                        fragmentTransaction.addToBackStack("to-do list");
+                        fragmentTransaction.commit();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Uh-Oh", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
